bdcc9886f558d32caae5de8049ccbd6b4b08aa6a
AbdulHaseeb28 <haseeb.intern@davsinc.com>
2023-08-24 16:49:14 +0500

    generating_models

---
+++ db/migrate/20230824112418_create_bugs.rb
@@ -0,0 +1,16 @@
+class CreateBugs < ActiveRecord::Migration[5.2]
+  def change
+    create_table :bugs do |t|
+      t.string :title
+      t.date :deadline
+      t.binary :screenshot
+      t.string :type
+      t.string :status
+      t.integer :user_id
+      t.integer :developer_id
+      t.integer :project_id
+
+      t.timestamps
+    end
+  end
+end
